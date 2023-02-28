b5b555a2e8cefd7b6e013a3ebe97d9eba2202a1f
alchambron <alexis.chambron@gmail.com>
2023-02-28 19:42:49 +0100

    update

--- ReactAPI/src/components/Home/Home.jsx
+++ ReactAPI/src/components/Home/Home.jsx
@@ -1,7 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 export default function Home() {
-  return (
-    <h1>Home</h1>
-  )
+  const [data, setData] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetchData() {
+      const resp = await fetch("http://localhost:3000/articles");
+      const fetchdata = await resp.json();
+      setData(fetchdata);
+      setIsLoading(false);
+    }
+    fetchData();
+  }, []);
+
+  return isLoading ? (
+    "Loading..."
+  ) : (
+    <>
+    <div>
+      <h1> Créer un nouveau post</h1>
+      <Link to="/posts/create">Nouveau</Link>
+    </div>
+      <div className="Content">
+        {data.map((value) => (
+          <>
+            <h1>{value.title}</h1>
+            <p>{value.content} </p>
+          </>
+        ))}
+      </div>
+    </>
+  );
 }
